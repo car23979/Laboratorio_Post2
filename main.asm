3a3ac334b669f61327ef867228ba85a8dffc6324
car23979 <125841463+car23979@users.noreply.github.com>
2025-02-20 12:41:36 -0600

    Tabla
    Se agrego una tabla de segmentos para el display anodo

--- main.asm
+++ main.asm
@@ -22,4 +22,6 @@ Descripción: Implementación del Timer0 y de un contador de segundos
 .def ESTADO_ANTERIOR = R21	// Último estado de botones
 .def ALARMA = R22			// Estado del LED
 
-//
+// Tabla de segmentos (ánodo común)
+
+TABLA: .db 0xC0,0xF9, 0xA4, 0xB0, 0x99, 0x92, 0x82, 0xF8, 0x80, 0x90, 0x88, 0x83, 0xC6, 0xA1, 0x86, 0x8E // Dígitos del 0 al 15
