1d719234f84b3e3e7c28745e514acaa50d025dce
car23979 <125841463+car23979@users.noreply.github.com>
2025-02-20 12:39:26 -0600

    Definición de registros
    Se escribieron los registros que se estaran usando para el circuito

--- main.asm
+++ main.asm
@@ -14,3 +14,12 @@ Descripción: Implementación del Timer0 y de un contador de segundos
 
 
 // Definición de registros
+
+.def CONTADOR_SEG = R18		// Contador de 1s (4 bits)
+.def CONTADOR_BOT = R19		// Contador hezadecimal (botones)
+.def OVERFLOW_100MS = R20	// Cuenta 10 desbordamientos = 1s
+.def TEMP = R16				// Registro Temporal
+.def ESTADO_ANTERIOR = R21	// Último estado de botones
+.def ALARMA = R22			// Estado del LED
+
+//
